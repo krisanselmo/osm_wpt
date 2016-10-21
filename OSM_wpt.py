fffa2c76fce785be714cf6757590021756dd1edb
krisanselmo <christophe.anselmo@gmail.com>
2016-10-21 15:17:22 +0200

    Add WPT to the closest position on the route

--- OSM_wpt.py
+++ OSM_wpt.py
@@ -7,8 +7,9 @@ Created on Mon Oct 17 15:28:58 2016
 
 TODO: 
     - Add the possibility to query ways in addition of nodes
-    - Insert new point close to the WPT on the WPT
-
+    - Keep old WPT (partially functional)
+    - (Issue) Insert new point close to the WPT on the WPT
+    
 """
 
 import gpxpy      #https://github.com/tkrajina/gpxpy
@@ -23,13 +24,15 @@ from math import radians, cos, sin, asin, sqrt
 
 
 class Point(object):
-    def __init__(self, name, lat, lon, ele, node_id, query_name):
+    def __init__(self, name, lon, lat, ele, node_id, index, new_gpx_index, query_name):
         self.name = name
         self.lat = lat
         self.lon = lon
         
         self.query_name = query_name
-        self.node_id = node_id
+        self.OSM_node_id = node_id
+        self.index = index
+        self.new_gpx_index = new_gpx_index
         
         if (ele == ''):
             self.ele = 0
@@ -40,7 +43,7 @@ class Point(object):
                self.ele = 0
                
     def __repr__(self):
-        return repr((self.node_id, self.query_name, self.name, self.lat, self.lon, self.ele))
+        return repr((self.OSM_node_id, self.index, self.new_gpx_index, self.query_name, self.name, self.lat, self.lon, self.ele))
         
         
 def Parse_route(gpx):
@@ -71,7 +74,7 @@ def Plot_gpx_wpt(gpx):
         lon = waypoint.longitude
         lat = waypoint.latitude
         print 'waypoint {0} -> ({1},{2})'.format(waypoint.name.encode('utf-8'), lat, lon)
-        plt.plot(lon, lat, 'ro')
+        plt.plot(lon, lat, 'yo')
 
 
 def plot_gpx_route(lon,lat,title):
@@ -81,26 +84,36 @@ def plot_gpx_route(lon,lat,title):
     ax.set_axis_off()
     ax.set_title(title,color='white', fontsize=15)
     fig.add_axes(ax)
-    plt.plot(lon, lat, color = 'red', lw = 1, alpha = 1)
+    plt.plot(lon, lat, '+-', color = 'red', lw = 1, alpha = 1)
     plt.hold(True)
     return plt
         
     
-def Get_Overpass_Feature(Pts,index_used,lat,lon,query_name):
+def Plot_Overpass_Feature():
+    tree = ET.parse("Overpass.xml")
+    root = tree.getroot()
+    allnodes=root.findall('node')
+    for node in allnodes:
+        lat2 = float(node.get('lat'))
+        lon2 = float(node.get('lon'))
+        plt.plot(lon2,lat2,'g+')
+    
+def Get_Overpass_Feature(Pts,index_used,lat,lon,lim_dist,query_name):
     tree = ET.parse("Overpass.xml")
     root = tree.getroot()
     allnodes=root.findall('node')
     i_name = 1
     
     for node in allnodes:
-        lat2 = node.get('lat')
-        lon2 = node.get('lon')
+        lat2 = float(node.get('lat'))
+        lon2 = float(node.get('lon'))
         node_id = node.get('id')
         
-        (match, near_lon, near_lat,index) = Find_nearest(lon,lat,float(lon2),float(lat2))  
+        (match, near_lon, near_lat,index) = Find_nearest(lon,lat,lon2,lat2,lim_dist)  
         
         if (match == 1):
-#                    
+            
+            [lon_new,lat_new, new_gpx_index] = add_new_point(lon,lat,lon2,lat2,index)         
             name = query_name + str(i_name) # set default in case proper tag not found
             ele = '' # set default in case proper tag not found
             
@@ -113,30 +126,69 @@ def Get_Overpass_Feature(Pts,index_used,lat,lon,query_name):
             # Because only 1 POI is possible per GPS point         
             if index not in index_used: 
                 print query_name + " - " + name + " - " + ele 
-                Pt = Point(name, near_lat, near_lon, ele, node_id, query_name)
+                Pt = Point(name, lon_new, lat_new, ele, node_id, index, new_gpx_index, query_name)
                 Pts.append(Pt)             
                 index_used.append(index)
                 i_name = i_name + 1
-    return Pts     
+    return Pts
     
-    
-def Find_nearest(lon,lat,lon2,lat2):
+
+def Find_nearest(lon,lat,lon2,lat2,lim_dist):
     """
     Find if an OSM node matches with the gpx route and return the nearest 
     coordinates and its index 
     """
     dist = []
     match = 0
-    lim_dist = 0.05 # in kilometers
+    
     for i in range(len(lat)):  
         d = haversine(lon[i], lat[i], lon2, lat2)
         dist.append(d)
     i = dist.index(min(dist))
     if (min(dist) < lim_dist):  
         match = 1
+        print 'Distance to node: ' + str(min(dist)*1e3) + ' m'
     return(match, lon[i], lat[i], i)
 
 
+def add_new_point(lon,lat,lon2,lat2,index):
+    if (index == 0) or (index+1 == len(lat)) :
+        return None,None,None
+        
+    d_prev = haversine(lon[index-1], lat[index-1], lon2, lat2)
+    d_next = haversine(lon[index+1], lat[index+1], lon2, lat2)
+
+    if (d_prev < d_next):
+        i = index-1
+    else:
+        i = index+1
+
+    [lon_new,lat_new,exist] = get_perp( lon[i], lat[i], lon[index], lat[index], lon2, lat2)
+    if exist == 1:
+        i = index
+    return lon_new,lat_new, i
+
+def get_perp( X1, Y1, X2, Y2, X3, Y3):
+    """
+    Purpose - X1,Y1,X2,Y2 = Two points representing the ends of the line 
+    segment
+              X3,Y3 = The offset point 
+    'Returns - X4,Y4 = Returns the Point on the line perpendicular to the
+    offset or None if no such point exists
+    """
+    XX = X2 - X1 
+    YY = Y2 - Y1 
+    if (XX*XX) + (YY*YY) == 0:
+        return X2,Y2,1
+    ShortestLength = ((XX*(X3 - X1)) + (YY*(Y3 - Y1)))/((XX*XX) + (YY*YY)) 
+    X4 = X1 + XX * ShortestLength 
+    Y4 = Y1 + YY * ShortestLength
+#    if X4 < X2 and X4 > X1 and Y4 < Y2 and Y4 > Y1:
+    return X4,Y4,0
+#    else:
+#        return X2,Y2,1
+    
+    
 def haversine(lon1, lat1, lon2, lat2):
     """
     Calculate the great circle distance between two points 
@@ -145,7 +197,6 @@ def haversine(lon1, lat1, lon2, lat2):
     """
     # convert decimal degrees to radians 
     lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
-
     # haversine formula 
     dlon = lon2 - lon1 
     dlat = lat2 - lat1 
@@ -156,13 +207,13 @@ def haversine(lon1, lat1, lon2, lat2):
     
     
 def Overpass_Query(lon, lat, query):
-    margin = 0.01
+    margin = 0.001
     minlon = min(lon) - margin
     maxlon = max(lon) + margin
     minlat = min(lat) - margin
     maxlat = max(lat) + margin
 #    api = overpass.API()
-#    par défaut: http://overpass-api.de/api/interpreter
+#   Default : http://overpass-api.de/api/interpreter
     api = overpass.API(endpoint='http://api.openstreetmap.fr/oapi/interpreter')
     
     pos_str = str(minlat) + ',' + str(minlon) + ',' +\
@@ -191,22 +242,39 @@ def save_XML(fname,response):
     f.close()    
     
     
-def Create_gpx(gpx_data,Pts):
+def Create_gpx(gpx_data,Pts,keep_old_WPT = True):
 
     gpx = gpxpy.gpx.GPX()
 
-    for route in gpx_data.routes:
-        gpx.routes.append(route)
-        
-    for track in gpx_data.tracks:
-        gpx.tracks.append(track)    
-        
-    for waypoint in gpx_data.waypoints:    
-        gpx.waypoints.append(waypoint)
+    # Create first track in our GPX: 
+    gpx_track = gpxpy.gpx.GPXTrack() 
+    gpx.tracks.append(gpx_track) 
+
+    gpx_segment = gpxpy.gpx.GPXTrackSegment()
+    gpx_track.segments.append(gpx_segment)
+    
+    for i in range(len(lat)): 
+        if (i in index_used):
+            Pt = filter(lambda Pt: Pt.index == i, Pts)
+            P = Pt[0]
+            if (P.new_gpx_index < i) and P.new_gpx_index is not None:
+                gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(P.lat, P.lon, elevation=ele[i])) 
+        gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(lat[i], lon[i], elevation=ele[i])) 
+        if i in index_used:
+            
+            if (P.new_gpx_index > i) and P.new_gpx_index is not None:
+                gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(P.lat, P.lon, elevation=ele[i])) 
+
+    if keep_old_WPT is True :
+        for waypoint in gpx_data.waypoints:    
+            gpx.waypoints.append(waypoint)
     
     for Pt in Pts:
+        
+        ok = filter(lambda wpt: round(wpt.latitude*1e5) == round(Pt.lat*1e5), gpx_data.waypoints)
+        if len(ok) == 0 :       
 #        gpx.waypoints.append(gpxpy.gpx.GPXWaypoint(Pt.lat, Pt.lon, elevation=Pt.ele, name=Pt.name, comment=Pt.query_name, symbol=Pt.query_name, type=Pt.query_name))
-        gpx.waypoints.append(gpxpy.gpx.GPXWaypoint(Pt.lat, Pt.lon, elevation=Pt.ele, name=Pt.name, symbol=Pt.query_name, type=Pt.query_name))
+            gpx.waypoints.append(gpxpy.gpx.GPXWaypoint(Pt.lat, Pt.lon, elevation=Pt.ele, name=Pt.name, symbol=Pt.query_name, type=Pt.query_name))
     f = open('out.gpx', "wb")
     f.write(gpx.to_xml())
     f.close()    
@@ -214,8 +282,12 @@ def Create_gpx(gpx_data,Pts):
     
 if __name__ == "__main__":
 
+    # -----------------------------
     fpath = 'test.gpx'
-    
+    lim_dist = 0.05 # in kilometers
+    keep_old_WPT = False
+	# -----------------------------
+
     gpx_file = open(fpath, 'r')
     gpx = gpxpy.parse(gpx_file)
     (gpx_name,lat,lon,ele) = Parse_route(gpx)
@@ -223,56 +295,50 @@ if __name__ == "__main__":
     
     index_used = []
     Pts = []
+
     
     query = '"natural" = "saddle"'
     Overpass_Query(lon, lat, query)
-    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon, 'saddle')
+    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon,lim_dist, 'saddle')
  
     query = '"natural" = "peak"'
     Overpass_Query(lon, lat, query)
-    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon, 'peak')
+    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon,lim_dist, 'peak')
      
     query = '"waterway"="waterfall"'
     Overpass_Query(lon, lat, query)
-    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon, 'waterfall')
+    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon,lim_dist, 'waterfall')
     
     query = '"information"="guidepost"'
     Overpass_Query(lon, lat, query)
-    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon, 'guidepost')
+    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon,lim_dist, 'guidepost')
     	
     query = '"natural"="cave_entrance"'
     Overpass_Query(lon, lat, query)
-    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon, 'cave_entrance')
+    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon,lim_dist, 'cave_entrance')
     
     query = '"tourism"="viewpoint"'
     Overpass_Query(lon, lat, query)
-    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon, 'viewpoint')
-	    	
+    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon,lim_dist, 'viewpoint')
+    	
     query = '"amenity"="drinking_water"'
     Overpass_Query(lon, lat, query)
-    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon, 'water') 
+    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon,lim_dist, 'water') 
     
     query = '"tourism"="alpine_hut"'
     Overpass_Query(lon, lat, query)
-    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon, 'alpine_hut') 
+    Pts = Get_Overpass_Feature(Pts,index_used,lat,lon,lim_dist, 'alpine_hut') 
 
 
     plot_gpx = 0
     if (plot_gpx == 1):
         plot_gpx_route(lon,lat,gpx_name)   # Plot route 
         Plot_gpx_wpt(gpx)                  # Plot waypoints from the input gpx
+        Plot_Overpass_Feature() 
         for Pt in Pts:
             plt.plot(Pt.lon, Pt.lat, 'bo') # Plot new waypoints
         plt.show()
-    
-    
+        
     print 'Number of gpx points in route : ' + str(len(lat))
     print str(len(index_used)) + ' Waypoint(s)'
-    Create_gpx(gpx,Pts)
-
-    
-    
-    
-
-    
-    
+    Create_gpx(gpx,Pts,keep_old_WPT)
